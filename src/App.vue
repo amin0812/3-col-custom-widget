8fdc186ca5e5ec4322f801c1aab3913ba39242a7
Elaoud, Mohamed Amin <Mohamed-Amin.Elaoud@adesso.de>
2024-09-04 12:13:26 +0200

    DWS button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <Vueform :key="updateKey" v-bind="vueform" />
 
-  <div id="widget-content" style="width: 800px; max-width: 100%; margin: 0 auto; background-color: #f0f0f0; padding: 10px; border-radius: 5px;">
+  <div id="widget-content"
+    style="width: 800px; max-width: 100%; margin: 0 auto; background-color: #f0f0f0; padding: 10px; border-radius: 5px;">
     <table style="width: 100%; border-collapse: collapse; table-layout: fixed;">
-      <tr>
-      <td colspan="2" style="text-align: left;">
-        <h2 style="margin: 0 0 5px 0;">{{ formValues.headline }}</h2>
-        <h4 style="margin: 0 0 10px 0; color: #555;">{{ formValues.subheadline }}</h4>
-      </td>
-    </tr>
       <tbody>
+        <tr>
+          <td style="text-align: left;">
+            <h2 class="h3-purple"
+              style="font-size: 1.5em; color:#6D5BA3;text-align: left; font-family: 'Times New Roman', Times, serif;">
+              {{ formValues.headline }}
+            </h2>
+            <h4 class="h3-black"
+              style="font-size: 1.5em; color:#000;text-align: left; font-family: 'Times New Roman', Times, serif;">
+              {{ formValues.subheadline }}
+            </h4>
+          </td>
+        </tr>
         <tr>
           <!-- Loop through columns to create table cells -->
           <template v-for="(column, index) in columns" :key="index">
@@ -18,19 +25,36 @@
               <template v-if="column.image">
                 <div style="text-align: center; padding: 10px;">
                   <img :src="column.image" alt="Image" style="max-width: 100%; height: auto; object-fit: cover;" />
-                  <p v-if="column.text" style="margin: 10px auto; text-align: center; word-wrap: break-word;">
-                {{ column.text }}
-              </p>
+                  <p v-if="column.text"
+                    style="margin: 10px auto; text-align: justify; word-wrap: break-word; width: 100%;">
+                    {{ column.text }}
+                  </p>
                 </div>
               </template>
-              <!-- Ensure text width matches image width -->
-             
+
+              <!-- Custom button HTML structure -->
               <template v-if="column.button">
-                <a :href="column.buttonLink" style="text-decoration: none;">
-                  <button style="display: block; width: 100%; padding: 10px; background-color: #007bff; color: #fff; border: none; border-radius: 5px; cursor: pointer;">
-                    {{ column.button }}
-                  </button>
-                </a>
+                <div style="padding: 10px;">
+                  <table border="0" cellpadding="0" cellspacing="0" style="background:#ffffff; width: 100%;">
+                    <tr>
+                      <td alt="left-button" width="28px !important;">
+                        <img alt="" height="40"
+                          src="https://image.s50.sfmc-content.com/lib/fe3211717564047b7c1272/m/1/2cff3a47-c908-46e5-bf25-fcc44541a4d3.png"
+                          style="display: block; height: 40px !important; max-height: 40px !important;" width="28">
+                      </td>
+                      <td style="max-width:290px; background-color: #007d85;">
+                        <a :href="column.buttonLink"
+                          style="color:#ffffff;text-decoration:none;font-family: DWS Slab TT, Rockwell, Calibri, Arial, Helvetica, sans-serif;height:40px !important;max-height:40px !important;font-size: 14px;line-height: 45px;mso-line-height-rule: exactly;display: block;background: #007d85;background-color: #007d85;text-align: center;vertical-align: middle;"
+                          :title="column.button">{{ column.button }}</a>
+                      </td>
+                      <td alt="right-button" width="28px !important;">
+                        <img alt="" height="40"
+                          src="https://image.s50.sfmc-content.com/lib/fe3211717564047b7c1272/m/1/1d91c65d-47fb-4c8a-98e1-612c093749f7.png"
+                          style="display: block; height: 40px !important; max-height: 40px !important;" width="28">
+                      </td>
+                    </tr>
+                  </table>
+                </div>
               </template>
             </td>
           </template>
